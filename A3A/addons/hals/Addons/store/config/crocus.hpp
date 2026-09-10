3ccb8f88209586046cb8cef55e3848a0a9576b5f
Niklas <niklasdroll@web.de>
2026-09-10 14:23:14 +0200

    added all crocus drones

--- A3A/addons/hals/Addons/store/config/crocus.hpp
+++ A3A/addons/hals/Addons/store/config/crocus.hpp
@@ -1,10 +1,11 @@
         // Unsere CROCUS DRONEN
 		class crocus{
-			displayName = __EVAL(formatText ["%1 %2","CROCUS","CROCUS"]);
+			displayName = __EVAL(formatText ["%1 %2","CROCUS","DRONEN"]);
 			picture = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\handgun_ca.paa";
 
-			ITEM(B_Crocus_AP_TI_Bag, 110, MISC_STOCK); 
-			// ITEM(BWA3_MP7, 111, MISC_STOCK);
-			// ITEM(BWA3_MP7, 112, MISC_STOCK);
+			ITEM(I_Crocus_AP_TI_Bag, 3000, MISC_STOCK); 
+			ITEM(I_Crocus_AP_Bag, 3000, MISC_STOCK); 
+			ITEM(I_Crocus_AT_TI_Bag, 3000, MISC_STOCK); 
+			ITEM(I_Crocus_AT_Bag, 3000, MISC_STOCK); 
 		};
 

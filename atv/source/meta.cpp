cfafac903913a6f3250eec46cbbb381c4ee333cc
Niklas <niklasdroll@web.de>
2026-09-16 12:01:14 +0200

    copied in 6x6 ATV mod

---
+++ atv/source/meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 1190934425;
+name = "6 x 6 All Terrain Vehicle";
+timestamp = 5248146497520065743;
---
+++ atv/source/mod.cpp
@@ -0,0 +1,30 @@
+
+/// Alpha Version 0.0.2 01 NOV 17
+name = "Special Operations Vehicles [NDS]";
+author = "Neddles";
+/// Picture displayed from the Expansions menu
+picture = "Data\NDS_SOVmodpic.paa";
+/// Display next to the item added by the mod
+logoSmall = "Data\NDS_modlogo_s.paa";
+/// Logo displayed in the main menu
+logo = "Data\NDS_modlogo.paa";
+/// When the mouse is over, in the main menu
+logoOver = "Data\NDS_modlogo_g.paa";
+/// Website URL, that can accessed from the extansion menu
+action = "https://en.wikipedia.org/wiki/Special_forces";
+/// Tool tip displayed when the mouse is left over, in the main menu
+tooltipOwned = "It's yours!";
+/// Color used for DLC stripes and backgrounds
+dlcColor[] = 
+{
+	0.23, // Red
+	0.39, // Green
+	0.30, // Blue
+	1 // Alpha
+};
+/// Overview text, displayed from the extansion menu
+overview = "A collection of Special Operations themed Weapons and Vehicles. My idea is to include unique items not found in other mods. ";
+/// Hide the extansion name 
+hideName = 0;
+/// Hide the extansion menu
+hidePicture = 0;
